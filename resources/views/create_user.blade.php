01b6ab28b98b0ceacd8254b0c40f72e773b08f58
camilars <camila.rs122@gmail.com>
2018-10-30 12:05:21 -0300

    incluído validação de e-mail no cadastro de usuário

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -8,12 +8,37 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">  
     <script  data-src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+    
+    <script language="Javascript">
+        function validacaoEmail(field) {
+        usuario = field.value.substring(0, field.value.indexOf("@"));
+        dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+ 
+        if ((usuario.length >=1) &&
+            (dominio.length >=3) && 
+            (usuario.search("@")==-1) && 
+            (dominio.search("@")==-1) &&
+            (usuario.search(" ")==-1) && 
+            (dominio.search(" ")==-1) &&
+            (dominio.search(".")!=-1) &&      
+            (dominio.indexOf(".") >=1)&& 
+            (dominio.lastIndexOf(".") < dominio.length - 1)) {
+            document.getElementById("msgemail").innerHTML="E-mail válido";
+            alert("E-mail valido");
+        }
+          else{
+            document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
+      alert("E-mail invalido");
+}
+}
+</script>
+
   </head>
   <body>
     <div class="container">
       <h2>Cadastro de usuário</h2><br/>
-      <form method="post" action="{{url('users')}}" enctype="multipart/form-data">
+      <form method="post" action="{{url('users')}}" enctype="multipart/form-data" name="f1">
         @csrf
         <div class="row">
           <div class="col-md-4"></div>
@@ -33,8 +58,9 @@
           <div class="col-md-4"></div>
             <div class="form-group col-md-4">
               <label for="Email">Email:</label>
-              <input type="text" class="form-control" name="email">
+              <input type="text" class="form-control" name="email" onblur="validacaoEmail(f1.email)">
             </div>
+            <p id="msgemail"></p>
           </div>
         <div class="row">
           <div class="col-md-4"></div>
